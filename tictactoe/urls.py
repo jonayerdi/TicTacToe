e7c0402a6d9ab40f989a844b8f0e16ea61a4b727
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-12-17 22:43:57 +0100

    Register, Login and Logout

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^newuser/', views.create_user),
     url(r'^$', views.main_page, name='main_page'),
 	url(r'^game/(?P<pk>\d+)/$', views.game_page, name='game_page'),
 ]
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -2,15 +2,38 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .forms import PostForm
-from .models import Post
+from django.contrib.auth.models import User
+from django.db.models import Q
+from .models import Game
+from .forms import CreateUserForm
 
 def main_page(request):
-	users = User.objects.all()
-	user_games = Game.objects.filter(user1=request.user.pk | user2=request.user.pk).filter(is_finished=0).order_by('created_date')[:16]
-    games = Game.objects.filter(is_finished=0).order_by('created_date')[:16]
-    return render(request, 'tictactoe/main_page.html', {'users': users, 'user_games': user_games, 'games': games})
-	
+    users = User.objects.all().order_by('username')
+    challenges = Game.objects.filter(user2=request.user.pk).filter(outcome=-2).order_by('created_date')
+    user_games = Game.objects.filter(Q(user1=request.user.pk) | Q(user2=request.user.pk)).filter(Q(outcome=-2) | Q(outcome=-1)).order_by('created_date')
+    games = Game.objects.filter(Q(outcome=-2) | Q(outcome=-1)).order_by('created_date')[:64]
+    return render(request, 'tictactoe/main_page.html', {'users': users, 'challenges': challenges, 'user_games': user_games, 'games': games})
+
 def game_page(request, pk):
     game = get_object_or_404(Game, pk=pk)
     return render(request, 'tictactoe/game_page.html', {'game': game})
+    
+def create_user(request):
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            usr = form.save(commit=False)
+            user = User.objects.create_user(username=usr.username, password=usr.password)
+            return redirect('login')
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            userName = request.username
+            userPass = request.password
+            user = User.objects.create_user(username=userName, password=userPass)
+            return redirect('login')
+    else:
+	    form = CreateUserForm()
+    return render(request, 'registration/create_user.html', {'form': form})
+
+    
