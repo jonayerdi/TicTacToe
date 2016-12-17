77af03e16735cf42e0a62b07080f8465bf018d0c
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-12-17 20:20:16 +0100

    Front page template working

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -1,33 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.functions import Concat
 from django.utils import timezone
 
 class Game(models.Model):
-	id = models.AutoField(primary_key=True)
-    user1 = models.ForeignKey('auth.User')
-	user2 = models.ForeignKey('auth.User')
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    user1 = models.ForeignKey('auth.User', related_name='+')
+    user2 = models.ForeignKey('auth.User', related_name='+')
+    state = models.CharField(max_length=36)
     created_date = models.DateTimeField(default=timezone.now)
     finished_date = models.DateTimeField(blank=True, null=True)
-	is_finished = models.BooleanField(default=0)
-	outcome = models.IntegerField(default=-1)
+    outcome = models.IntegerField(default=-2)
+    
+    def setName(self):
+        self.name = Concat(User.objects.get(pk=user1).username," vs ",ser.objects.get(pk=user2).username)
+        self.save()
 	
     def finish(self, winner):
-		if winner == self.user1:
-			self.outcome = 1
-		elif winner == self.user2:
-			self.outcome = 2
-		else:
-			self.outcome = 0
+        if winner == self.user1:
+            self.outcome = 1
+        elif winner == self.user2:
+            self.outcome = 2
+        else:
+            self.outcome = 0
         self.finished_date = timezone.now()
-		self.is_finished = 1
+        self.is_finished = 1
         self.save()
-		
-	def winner(self):
-		switch = {
-			0: null,
-			1: user1,
-			2: user2,
-		}
-		return switch.get(self.outcome, null)
+        
+    def winner(self):
+        switch = {
+            0: null,
+            1: user1,
+            2: user2,
+        }
+        return switch.get(self.outcome, null)
 
     def __str__(self):
-        return self.id
+        return self.name
