b0feef8416bd02a161819d3122f654296f21944a
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-12-19 20:29:59 +0100

    Migrated to PostgreSQL

--- assignment2/settings.py
+++ assignment2/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'assignment2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tictactoe',                      
+        'USER': 'django_tictactoe',
+        'PASSWORD': 'django_password',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
